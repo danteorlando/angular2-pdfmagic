5da69caaa5299feac7336b405065fc42e1e16aac
danteorlando <danteorlando@users.noreply.github.com>
2017-08-11 16:02:11 -0700

    removing .js and .js.map files

--- src/app/formfield.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var FormField = (function () {
-    function FormField() {
-    }
-    return FormField;
-}());
-exports.FormField = FormField;
-//# sourceMappingURL=formfield.js.map
